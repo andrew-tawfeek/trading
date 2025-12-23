9ec0e5bc474a2a16af27cee8da18a43cd28e5263
Andrew Tawfeek <atawfeek.math@gmail.com>
2025-12-22 16:22:49 -0800

    experiment refinement

--- experiments.py
+++ experiments.py
@@ -4,19 +4,26 @@ from fourier import run_fourier_options_backtest
 def fourier_options_parameter_search(ticker: str,
                                      start_date: str,
                                      end_date: str,
-                                     tick_size: str = '1d'
+                                     tick_size: str = '1d',
+                                     n_harmonics_range: list = list(range(1, 25)),
+                                     smoothing_options: list = [0, 2, 4],
+                                     overbought_range: list = list(range(2, 10)),
+                                     oversold_range: list = list(range(-10, -2)),
+                                     otm_percent_options: list =[2.0, 5.0, 10.0],
+                                     stoploss_options: list =[10, 20, 30, 40, 50],
+                                     takeprofit_options: list =[10, 20, 30, 40, 50],
                                      ):
 
     best_return = -100
     best_params = None
 
-    for n_harmonics in range(1, 30):
-        for smoothing in [0, 2, 4]:
-            for overbought in range(2, 10):
-                for oversold in range(-10, -2):
-                    for otm_percent in [2.0, 5.0, 10.0]:
-                        for stoploss in [10, 20, 30, 40, 50]:
-                            for takeprofit in [10, 20, 30, 40, 50]:
+    for n_harmonics in n_harmonics_range:
+        for smoothing in smoothing_options:
+            for overbought in overbought_range:
+                for oversold in oversold_range:
+                    for otm_percent in otm_percent_options:
+                        for stoploss in stoploss_options:
+                            for takeprofit in takeprofit_options:
                                 results = run_fourier_options_backtest(
                                     ticker=ticker,
                                     start_date=start_date,
